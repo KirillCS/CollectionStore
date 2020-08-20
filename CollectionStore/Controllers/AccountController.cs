15456f9b63955c75cf6ec2dadfd3a59160ed1926
KirillCS <KirillCS@users.noreply.github.com>
2020-08-20 21:02:07 +0300

    Action "SignUp" was added

---
+++ CollectionStore/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CollectionStore.Models;
+using CollectionStore.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectionStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<User> userManager;
+        private SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult SignUp() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new User { UserName = model.UserName, Email = model.Email };
+                var result = await userManager.CreateAsync(user, model.Password);
+                if(result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
