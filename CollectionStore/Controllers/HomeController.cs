93edf9999d08c8b72084d7c25782e9cd349e72d5
KirillCS <KirillCS@users.noreply.github.com>
2020-08-20 13:17:36 +0300

    _Layout was changed.

--- CollectionStore/Controllers/HomeController.cs
+++ CollectionStore/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace CollectionStore.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
