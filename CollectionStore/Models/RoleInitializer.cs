aaff08b131615771888e0aefe60a88509d6b98a5
KirillCS <KirillCS@users.noreply.github.com>
2020-08-24 13:31:33 +0300

    Data initialization was added

---
+++ CollectionStore/Models/RoleInitializer.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollectionStore.Models
+{
+    public class DataInitializer
+    {
+        private readonly UserManager<User> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IConfiguration configuration;
+        private const string AdminRoleName = "admin";
+        private const string UserRoleName = "user";
+        private string adminName;
+        private string adminPassword;
+
+        public DataInitializer(UserManager<User> userManager,
+            RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+            this.configuration = configuration;
+
+            adminName = configuration["Admin:Login"] ?? "Admin";
+            adminPassword = configuration["Admin:Password"] ?? "Qwe123!";
+        }
+
+        public async Task InitializeRoleAsync()
+        {
+            await CreateRoleIfNull(AdminRoleName, UserRoleName);
+        }
+        public async Task InitializeAdminAsync()
+        {
+            if((await userManager.FindByNameAsync(AdminRoleName)) == null)
+            {
+                var admin = new User { UserName = adminName };
+                var result = await userManager.CreateAsync(admin, adminPassword);
+                if(result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(admin, AdminRoleName);
+                }
+            }
+        }
+        private async Task CreateRoleIfNull (params string[] roles)
+        {
+            foreach (string role in roles)
+            {
+                if (!(await roleManager.RoleExistsAsync(role)))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
--- CollectionStore/Program.cs
+++ CollectionStore/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CollectionStore.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +14,25 @@ namespace CollectionStore
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var initializer = new DataInitializer(services.GetRequiredService<UserManager<User>>(),
+                        services.GetRequiredService<RoleManager<IdentityRole>>(), services.GetRequiredService<IConfiguration>());
+                    await initializer.InitializeRoleAsync();
+                    await initializer.InitializeAdminAsync();
+                }
+                catch (Exception ex)
+                {
+                    services.GetRequiredService<ILogger<Program>>().LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
