16f13400770359b80c01a0a03739451efe80fec3
KirillCS <KirillCS@users.noreply.github.com>
2020-08-28 22:46:46 +0300

    Error page was added

---
+++ CollectionStore/Controllers/ProfileController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CollectionStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollectionStore.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(string id = null)
+        {
+            if(id == null)
+            {
+            }
+            return View();
+        }
+    }
+}
--- CollectionStore/Models/ErrorViewModel.cs
+++ CollectionStore/Models/ErrorViewModel.cs
@@ -4,8 +4,7 @@ namespace CollectionStore.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public string ErrorTitle { get; set; }
+        public string ErrorMessage { get; set; }
     }
 }
