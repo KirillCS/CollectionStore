6e7b0d7ef3710d080febc14105038fd25543146d
KirillCS <potkirill20022603@gmail.com>
2020-09-01 23:51:00 +0300

    Markdown was added

---
+++ CollectionStore/wwwroot/js/markdown-editor.js
@@ -0,0 +1,6 @@
+﻿document.querySelectorAll(".js-markdown-input").forEach(input => {
+    const preview = input.closest(".js-markdown-block").querySelector(".js-markdown-preview");
+    input.addEventListener("input", e => {
+        preview.innerHTML = marked(input.value);
+    });
+});
