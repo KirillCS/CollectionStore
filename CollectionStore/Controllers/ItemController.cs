37d62bc71f74b2af108a69eb881c5de111cce058
KirillCS <KirillCS@users.noreply.github.com>
2020-09-06 22:38:22 +0300

    Files was added

---
+++ CollectionStore/Controllers/ItemController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CollectionStore.Data;
+using CollectionStore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollectionStore.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Add(int? collectionId = null, string returnUrl = null)
+        {
+            collectionId ??= -1;
+            var collection = context.Collections.Where(c => c.Id == collectionId).Include(c => c.Fields).ThenInclude(f => f.Type).SingleOrDefault(c => c.Id == collectionId);
+            if(collection == null)
+            {
+                return View("Error", new ErrorViewModel
+                {
+                    ErrorTitle = "Collection isn't found!",
+                    ErrorMessage = "Collection is not found!"
+                });
+            }
+            return View(new AddingItemViewModel
+            {
+                CollectionId = collectionId,
+                CollectionName = collection.Name,
+                Fields = collection.Fields,
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddingItemViewModel model)
+        {
+
+            return null;
+        }
+    }
+}
---
+++ CollectionStore/ViewModels/AddingItemViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CollectionStore.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollectionStore.ViewModels
+{
+    public class AddingItemViewModel
+    {
+        [Required(ErrorMessage = "Required")]
+        public string Name { get; set; }
+
+        public List<int?> FieldIds { get; set; }
+
+        public List<string> Values { get; set; }
+
+        public int? CollectionId { get; set; }
+
+        public string CollectionName { get; set; }
+
+        public List<Field> Fields { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
