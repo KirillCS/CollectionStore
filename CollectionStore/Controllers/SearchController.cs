a52690da7c3741d7eb42cbf7bd7b530aa24d293f
KirillCS <KirillCS@users.noreply.github.com>
2020-09-12 00:40:39 +0300

    Search was added

---
+++ CollectionStore/Controllers/SearchController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CollectionStore.Data;
+using CollectionStore.Models;
+using CollectionStore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollectionStore.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public SearchController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Search(string searchString = null)
+        {
+            searchString ??= string.Empty;
+            return View(new SearchViewModel 
+            {
+                SearchString = searchString,
+                Items = GetItems(searchString)
+            });
+        }
+
+        private List<Item> GetItems(string searchString)
+        {
+            var items = new List<Item>();
+            items = context.Items.Where(i => /*EF.Functions.FreeText(i.Name, searchString)*/i.Name.Contains(searchString)).ToList();
+            return items;
+        }
+    }
+}
---
+++ CollectionStore/ViewModels/SearchViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CollectionStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollectionStore.ViewModels
+{
+    public class SearchViewModel
+    {
+        public string SearchString { get; set; }
+
+        public List<Item> Items { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public SearchViewModel()
+        {
+            Items = new List<Item>();
+        }
+    }
+}
