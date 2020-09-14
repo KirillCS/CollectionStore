10f0bebfa89b5716fcfb8a0d03bf16a694f10cc3
KirillCS <KirillCS@users.noreply.github.com>
2020-09-14 22:12:53 +0300

    Services was added

---
+++ CollectionStore/Services/IEntityService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollectionStore.Services
+{
+    public enum OperationResult
+    {
+        Failed,
+        Successed
+    }
+
+    interface IEntityService<TEntity, TId>
+    {
+        TEntity GetById(TId id);
+        Task<OperationResult> AddAsync(TEntity entity);
+        Task<OperationResult> RemoveAsync(TId id);
+        Task<OperationResult> RemoveAsync(TEntity entity);
+        Task<OperationResult> UpdateAsync(TEntity entity);
+    }
+}
---
+++ CollectionStore/Services/ItemService.cs
@@ -0,0 +1,98 @@
+﻿using CollectionStore.Data;
+using CollectionStore.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollectionStore.Services
+{
+    public class ItemService : IEntityService<Item, int>
+    {
+        private readonly ApplicationDbContext context;
+
+        public ItemService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Item GetById(int id)
+        {
+            return context.Items.FirstOrDefault(i => i.Id == id);
+        }
+        public async Task<OperationResult> AddAsync(Item entity)
+        {
+            if (entity == null)
+            {
+                return OperationResult.Failed;
+            }
+            try
+            {
+                await context.Items.AddAsync(entity);
+                await context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                return OperationResult.Failed;
+            }
+            return OperationResult.Successed;
+        }
+        public async Task<OperationResult> RemoveAsync(int id)
+        {
+            var item = context.Items.FirstOrDefault(i => i.Id == id);
+            if(item != null)
+            {
+                return await RemoveAsync(item);
+            }
+            return OperationResult.Failed;
+        }
+        public async Task<OperationResult> RemoveAsync(Item entity)
+        {
+            if(entity == null)
+            {
+                return OperationResult.Failed;
+            }
+            try
+            {
+                RemoveItem(entity);
+                await context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                return OperationResult.Failed;
+            }
+            return OperationResult.Successed;
+        }
+        public async Task<OperationResult> UpdateAsync(Item entity)
+        {
+            var item = context.Items.FirstOrDefault(i => i.Id == entity.Id);
+            if (entity == null || item == null)
+            {
+                return OperationResult.Failed;
+            }
+            SetItem(item, entity);
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                return OperationResult.Failed;
+            }
+
+            return OperationResult.Successed;
+        }
+
+        private void RemoveItem(Item item)
+        {
+            context.FieldValues.RemoveRange(context.FieldValues.Where(fv => fv.ItemId == item.Id));
+            context.Items.Remove(item);
+        }
+        private void SetItem(Item item, Item sourceItem)
+        {
+            item.Name = sourceItem.Name;
+            item.CollectionId = sourceItem.CollectionId;
+            item.FieldValues = sourceItem.FieldValues;
+            item.ItemTags = sourceItem.ItemTags;
+        }
+    }
+}
