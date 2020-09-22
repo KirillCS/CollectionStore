0a953ca831b6548686e3faae86382eb94edd052e
KirillCS <potkirill20022603@gmail.com>
2020-09-22 12:27:24 +0300

    Returning to the previous tab was added

---
+++ CollectionStore/wwwroot/js/home-page-tabs.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(() => {
+    const tabId = sessionStorage.getItem("tabId");
+    if (tabId) {
+        $(`#${tabId}`).tab('show');
+    }
+
+    $('a[data-toggle="tab"]').on("show.bs.tab", e => sessionStorage.setItem("tabId", e.target.id));
+})
