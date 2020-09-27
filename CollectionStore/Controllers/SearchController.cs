41efcc8d46119cb904d73e3ba62414c16a2e2fc8
KirillCS <KirillCS@users.noreply.github.com>
2020-09-27 19:10:36 +0300

    Search button disabling was added

--- CollectionStore/Controllers/SearchController.cs
+++ CollectionStore/Controllers/SearchController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using CollectionStore.Data;
 using CollectionStore.Models;
 using CollectionStore.ViewModels;
