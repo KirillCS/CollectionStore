a5b25de940ed6cd84e85fea0c7d8401dabd6dc81
kirill_potapov <potkirill20022603@gmail.com>
2021-01-04 17:10:41 +0300

    View was created

---
+++ CollectionStore/ViewModels/StatisticsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CollectionStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollectionStore.ViewModels
+{
+    public class StatisticsViewModel
+    {
+        public List<User> TopUsersByCollections { get; set; }
+        public int CollectionsCount { get; set; }
+    }
+}
